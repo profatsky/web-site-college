5655b11ddc83205950b07d4bb00f9f89562ace52
Mikhail Profatsky <92920843+profatsky@users.noreply.github.com>
2022-06-16 17:44:01 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    /* Menu nav toggle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    /* Slider */
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
